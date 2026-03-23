315fddd68c81151149db27894216f5c19ae9dff1
jjllzhang <jalin.zhang.life@gmail.com>
2026-03-23 13:39:41 +0800

    refactor: share NTL parallel helpers

--- src/PCS/BaseFold/BaseFoldPCSInternal.hpp
+++ src/PCS/BaseFold/BaseFoldPCSInternal.hpp
@@ -9,12 +9,9 @@
 #include <vector>
 
 #include "PCS/BaseFold/BaseFoldPCS.hpp"
+#include "PCS/Common/NtlParallel.hpp"
 #include "PCS/Common/Profile.hpp"
 #include "PCS/Common/Transcript.hpp"
-
-#if defined(BASEFOLD_USE_OPENMP)
-#include <omp.h>
-#endif
 
 namespace basefold {
 namespace basefold_pcs_internal {
@@ -30,71 +27,11 @@ ProverCommitParallelConfig &MutableProverCommitParallelConfig();
 ProverCommitParallelConfig LoadProverCommitParallelConfig();
 long LoadEffectiveExtElementsPerThread();
 
-struct NtlThreadContextSnapshot {
-  NTL::ZZ base_modulus;
-  NTL::ZZ_pX extension_modulus;
-};
-
-inline NtlThreadContextSnapshot CaptureNtlThreadContextSnapshot() {
-  return {NTL::ZZ_p::modulus(), NTL::ZZ_pE::modulus().val()};
-}
-
-inline void InitNtlThreadContext(const NtlThreadContextSnapshot &ctx) {
-  NTL::ZZ_p::init(ctx.base_modulus);
-  NTL::ZZ_pE::init(ctx.extension_modulus);
-}
-
-inline int ChooseElementParallelThreads(long work_items, long parallel_threshold) {
-#if defined(BASEFOLD_USE_OPENMP)
-  if (parallel_threshold <= 0) {
-    parallel_threshold = 1;
-  }
-  if (work_items >= parallel_threshold) {
-    const int max_threads = omp_get_max_threads();
-    int threads_to_use = static_cast<int>(work_items / parallel_threshold);
-    if (threads_to_use > max_threads) {
-      threads_to_use = max_threads;
-    }
-    if (threads_to_use >= 2) {
-      return threads_to_use;
-    }
-  }
-#else
-  (void)work_items;
-  (void)parallel_threshold;
-#endif
-  return 1;
-}
-
-template <typename Fn>
-void ForEachIndexMaybeParallel(long begin, long end, long parallel_threshold,
-                               const Fn &fn) {
-  if (end <= begin) {
-    return;
-  }
-
-#if defined(BASEFOLD_USE_OPENMP)
-  const long work_items = end - begin;
-  const int threads_to_use =
-      ChooseElementParallelThreads(work_items, parallel_threshold);
-  if (threads_to_use >= 2) {
-    const NtlThreadContextSnapshot ntl_ctx = CaptureNtlThreadContextSnapshot();
-#pragma omp parallel num_threads(threads_to_use)
-    {
-      InitNtlThreadContext(ntl_ctx);
-#pragma omp for schedule(static)
-      for (long i = begin; i < end; ++i) {
-        fn(i);
-      }
-    }
-    return;
-  }
-#endif
-
-  for (long i = begin; i < end; ++i) {
-    fn(i);
-  }
-}
+using pcs_common_internal::CaptureNtlThreadContextSnapshot;
+using pcs_common_internal::ChooseElementParallelThreads;
+using pcs_common_internal::ForEachIndexMaybeParallel;
+using pcs_common_internal::InitNtlThreadContext;
+using pcs_common_internal::NtlThreadContextSnapshot;
 
 int ChooseQueryVerifyThreads(long num_queries);
 
